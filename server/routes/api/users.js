8470c6141794b451aa14d3a27bc784da9ea6c3dd
john2796 <jbmiranda22796@gmail.com>
2018-12-29 19:52:27 -0800

    created user registration and hash password with bcryptjs

--- server/routes/api/users.js
+++ server/routes/api/users.js
@@ -1,9 +1,53 @@
+// User API Routes % JWT Authentication
+
 const express = require("express");
 const router = express.Router();
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
+
+// Load User Model
+const User = require("../../models/User");
 
 // @route  GET api/users/test
 // @desc   Tests users route
 // @access Public
 router.get("/test", (req, res) => res.json({ msg: "users Works" }));
+
+// User Registation
+
+// @route  GET api/users/register
+// @desc   Register user
+// @access Public
+router.post(`/register`, (req, res) => {
+  //Find if email exists
+  User.findOne({ email: req.body.email }).then(user => {
+    if (user) {
+      return res.status(400).json({ email: "Email already exists" });
+    } else {
+      const avatar = gravatar.url(req.body.email, {
+        s: "200", //size
+        r: "pg", //rating pg
+        d: "mm" //default mm
+      });
+      const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        avatar,
+        password: req.body.password
+      });
+      // hash encrypt password using bcryptjs
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
+          newUser.password = hash;
+          newUser
+            .save()
+            .then(user => res.json(user))
+            .catch(err => console.log(err));
+        });
+      });
+    }
+  });
+});
 
 module.exports = router;
